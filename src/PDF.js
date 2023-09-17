c6c9b61c13f940b86f9c5d4dbca3207c983302de
AVVKavvk <kumawatvipin066@gmail.com>
2023-09-17 21:49:07 +0530

    ne

--- src/PDF.js
+++ src/PDF.js
@@ -222,209 +222,265 @@ const PDF = () => {
   };
 
   return (
-    <div className="max-w-[1200px] mx-auto flex flex-col justify-center py-5 items-center flex-wrap">
-      <h1 className="font-semibold text-2xl">Resume Generator</h1>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={e => handleInputChange(e, setName)}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="phoneNumber">Phone Number:</label>
-        <input
-          type="text"
-          id="phoneNumber"
-          value={phoneNumber}
-          onChange={e => handleInputChange(e, setPhoneNumber)}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="text"
-          id="email"
-          value={email}
-          onChange={e => handleInputChange(e, setEmail)}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="linkedinUrl">LinkedIn:</label>
-        <input
-          type="text"
-          id="linkedinUrl"
-          value={linkedinUrl}
-          onChange={handleLinkedinInputChange}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="college">College Name:</label>
-        <input
-          type="text"
-          id="college"
-          value={college}
-          onChange={e => handleInputChange(e, setCollege)}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="stream">Stream:</label>
-        <input
-          type="text"
-          id="stream"
-          value={stream}
-          onChange={e => handleInputChange(e, setStream)}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="cgpa">CGPA:</label>
-        <input
-          type="text"
-          id="cgpa"
-          value={cgpa}
-          onChange={e => handleInputChange(e, setCGPA)}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="collegeCity">College City:</label>
-        <input
-          type="text"
-          id="collegeCity"
-          value={collegeCity}
-          onChange={e => handleInputChange(e, setCollegeCity)}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="collegeCountry">College Country:</label>
-        <input
-          type="text"
-          id="collegeCountry"
-          value={collegeCountry}
-          onChange={e => handleInputChange(e, setCollegeCountry)}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="degreeCompletionYear">Degree Completion Year:</label>
-        <input
-          type="text"
-          id="degreeCompletionYear"
-          value={degreeCompletionYear}
-          onChange={e => handleInputChange(e, setDegreeCompletionYear)}
-          className="border border-gray-300 rounded p-2"
-        />
-      </div>
-      <div>
-        <label htmlFor="skills">Skills:</label>
-        {skills.map((skill, index) => (
-          <input
-            key={index}
-            type="text"
-            value={skill}
-            onChange={e => handleSkillInputChange(e, index)}
-            className="border border-gray-300 rounded p-2"
-          />
-        ))}
-      </div>
-      <div>
-        <label htmlFor="ProgrammingLanguage">Programming Language:</label>
-        {ProgrammingLanguage.map((language, index) => (
-          <input
-            key={index}
-            type="text"
-            value={language}
-            onChange={e => handleLanguageInputChange(e, index)}
-            className="border border-gray-300 rounded p-2"
-          />
-        ))}
-      </div>
-      <div>
-        {achievements.map((achievement, index) => (
-          <div key={index}>
-            <label>Achievement {index + 1}:</label>
-            <textarea
-              placeholder="Achievement"
-              value={achievement}
-              onChange={e => handleAchievementInputChange(e, index)}
-              rows="4"
-              cols="50"
-              className="border border-gray-300 rounded p-2"
-            ></textarea>
+    <div className="max-w-[1400px] mx-auto flex flex-col space-y-10 mb-5 justify-center py-5 items-center flex-wrap">
+      <h1 className="font-semibold text-2xl mb-5">Resume Generator</h1>
+      <div class="w-full mx-auto grid  sm:grid-cols-2 md:grid-cols-4  space-y-6 justify-center items-center flex-wrap">
+        <div class="w-[300px] mx-auto flex flex-col justify-center items-center border p-5 rounded-md  border-blue-500">
+          <h1 class="text-2xl text-violet-800  mb-4 mt-2">
+            Personal Information{" "}
+          </h1>
+          <div class=" w-[250px]  mx-auto grid grid-cols-1 gap-4  justify-center py-5 items-center flex-wrap">
+            <div class="flex flex-col justify-center gap-2 mx-auto w-full">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={e => handleInputChange(e, setName)}
+                className="border border-gray-300 rounded p-2"
+              />
+            </div>
+            <div class="flex flex-col justify-center gap-2 mx-auto w-full">
+              <label htmlFor="phoneNumber">Phone No:</label>
+              <input
+                type="text"
+                id="phoneNumber"
+                value={phoneNumber}
+                onChange={e => handleInputChange(e, setPhoneNumber)}
+                className="border border-gray-300 rounded p-2"
+              />
+            </div>
+            <div class="flex flex-col justify-center gap-2 mx-auto w-full">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={e => handleInputChange(e, setEmail)}
+                className="border border-gray-300 rounded p-2"
+              />
+            </div>
+            <div class="flex flex-col justify-center gap-2 mx-auto w-full">
+              <label htmlFor="linkedinUrl">LinkedIn:</label>
+              <input
+                type="text"
+                id="linkedinUrl"
+                value={linkedinUrl}
+                onChange={handleLinkedinInputChange}
+                className="border border-gray-300 rounded p-2"
+              />
+            </div>
           </div>
-        ))}
+        </div>
+        <div class="w-[300px] mx-auto flex flex-col justify-center items-center border p-5 rounded-md border-blue-500 ">
+          <h1 class="text-2xl text-violet-800  mb-4 mt-2">
+            Educational Information{" "}
+          </h1>
+          <div class="flex flex-col  justify-center gap-2 mx-auto w-full">
+            <label htmlFor="college">College Name:</label>
+            <input
+              type="text"
+              id="college"
+              value={college}
+              onChange={e => handleInputChange(e, setCollege)}
+              className="border border-gray-300 rounded p-2"
+            />
+          </div>
+          <div class="flex flex-col justify-center gap-2 mx-auto w-full">
+            <label htmlFor="stream">Stream:</label>
+            <input
+              type="text"
+              id="stream"
+              value={stream}
+              onChange={e => handleInputChange(e, setStream)}
+              className="border border-gray-300 rounded p-2"
+            />
+          </div>
+          <div class="flex flex-col justify-center gap-2 mx-auto w-full">
+            <label htmlFor="cgpa">CGPA:</label>
+            <input
+              type="text"
+              id="cgpa"
+              value={cgpa}
+              onChange={e => handleInputChange(e, setCGPA)}
+              className="border border-gray-300 rounded p-2"
+            />
+          </div>
+          <div class="flex flex-col justify-center gap-2 mx-auto w-full">
+            <label htmlFor="collegeCity">College City:</label>
+            <input
+              type="text"
+              id="collegeCity"
+              value={collegeCity}
+              onChange={e => handleInputChange(e, setCollegeCity)}
+              className="border border-gray-300 rounded p-2"
+            />
+          </div>
+          <div class="flex flex-col justify-center gap-2 mx-auto w-full">
+            <label htmlFor="collegeCountry">College Country:</label>
+            <input
+              type="text"
+              id="collegeCountry"
+              value={collegeCountry}
+              onChange={e => handleInputChange(e, setCollegeCountry)}
+              className="border border-gray-300 rounded p-2"
+            />
+          </div>
+          <div class="flex flex-col justify-center gap-2 mx-auto w-full">
+            <label htmlFor="degreeCompletionYear">
+              Degree Completion Year:
+            </label>
+            <input
+              type="text"
+              id="degreeCompletionYear"
+              value={degreeCompletionYear}
+              onChange={e => handleInputChange(e, setDegreeCompletionYear)}
+              className="border border-gray-300 rounded p-2"
+            />
+          </div>
+        </div>
+        <div class="w-[300px] mx-auto flex flex-col  justify-center items-center border p-5 rounded-md border-blue-500 ">
+          <label htmlFor="skills" class="text-2xl text-violet-800  mb-4 ">
+            Skills
+          </label>
+          <div class="flex flex-col  justify-center gap-[0.58rem] mx-auto w-full">
+            {skills.map((skill, index) => {
+              return (
+                <input
+                  key={index}
+                  type="text"
+                  value={skill}
+                  placeholder="Optional"
+                  onChange={e => handleSkillInputChange(e, index)}
+                  className="border border-gray-300 rounded p-2"
+                />
+              );
+            })}
+          </div>
+        </div>
+
+        <div class="sm:w-[330px] w-[300px] mx-auto flex flex-col  justify-center items-center border p-5 rounded-md border-blue-500 ">
+          <label
+            htmlFor="ProgrammingLanguage"
+            class="text-2xl text-violet-800  mb-4 "
+          >
+            Programming Language
+          </label>
+          <div class="flex flex-col  justify-center gap-[0.58rem] mx-auto w-full">
+            {ProgrammingLanguage.map((language, index) => (
+              <input
+                key={index}
+                type="text"
+                value={language}
+                placeholder="Optional"
+                onChange={e => handleLanguageInputChange(e, index)}
+                className="border border-gray-300 rounded p-2"
+              />
+            ))}
+          </div>
+        </div>
       </div>
 
-      <div>
-        {projects.map((project, index) => (
-          <div key={index}>
-            <label>Project {index + 1}:</label>
-            <input
-              type="text"
-              placeholder="Title"
-              value={project.title}
-              onChange={e =>
-                handleProjectInputChange(index, "title", e.target.value)
-              }
-              className="border border-gray-300 p-2"
-            />
-            <textarea
-              placeholder="Description"
-              value={project.description}
-              onChange={e =>
-                handleProjectInputChange(index, "description", e.target.value)
-              }
-              rows="4"
-              cols="50"
-              className="border border-gray-300 p-2"
-            ></textarea>
-            <input
-              type="text"
-              placeholder="GitHub Link"
-              value={project.githubLink}
-              onChange={e =>
-                handleProjectInputChange(index, "githubLink", e.target.value)
-              }
-              className="border border-gray-300 p-2"
-            />
-            <input
-              type="text"
-              placeholder="Project Link"
-              value={project.projectLink}
-              onChange={e =>
-                handleProjectInputChange(index, "projectLink", e.target.value)
-              }
-              className="border border-gray-300 p-2"
-            />
-            {projects.length > 1 && (
-              <button
-                onClick={() => removeProject(index)}
-                className="bg-red-500 text-white px-2 py-1 mt-2"
+      <div class="w-full mx-auto flex md:flex-row  space-y-5 justify-between items-center flex-wrap">
+        <div class=" md:w-auto   w-[300px] mx-auto flex flex-col  justify-center   border p-5 rounded-md border-blue-500 ">
+          <h1 class="text-2xl text-violet-800  mb-7 ">Projects</h1>
+          <div class="grid md:grid-cols-3  gap-5">
+            {projects.map((project, index) => (
+              <div
+                key={index}
+                class=" flex flex-col border my-5 w-[240px]  border-green-400 p-5 rounded justify-center gap-4 items-start"
               >
-                Remove Project
-              </button>
-            )}
+                <label>Project {index + 1}:</label>
+                <input
+                  type="text"
+                  placeholder="Title"
+                  value={project.title}
+                  onChange={e =>
+                    handleProjectInputChange(index, "title", e.target.value)
+                  }
+                  className="border border-gray-300 p-2 w-full"
+                />
+                <textarea
+                  placeholder="Description"
+                  value={project.description}
+                  onChange={e =>
+                    handleProjectInputChange(
+                      index,
+                      "description",
+                      e.target.value
+                    )
+                  }
+                  // rows="4"
+                  // cols="50"
+                  className="border border-gray-300 p-2 w-full"
+                ></textarea>
+                <input
+                  type="text"
+                  placeholder="GitHub Link"
+                  value={project.githubLink}
+                  onChange={e =>
+                    handleProjectInputChange(
+                      index,
+                      "githubLink",
+                      e.target.value
+                    )
+                  }
+                  className="border border-gray-300 p-2 w-full"
+                />
+                <input
+                  type="text"
+                  placeholder="Project Link"
+                  value={project.projectLink}
+                  onChange={e =>
+                    handleProjectInputChange(
+                      index,
+                      "projectLink",
+                      e.target.value
+                    )
+                  }
+                  className="border border-gray-300 p-2 w-full"
+                />
+                {projects.length > 1 && (
+                  <button
+                    onClick={() => removeProject(index)}
+                    className="bg-red-500 text-white px-2 py-1 mt-2"
+                  >
+                    Remove Project
+                  </button>
+                )}
+              </div>
+            ))}
           </div>
-        ))}
-        {projects.length < 3 && (
-          <button
-            onClick={addProject}
-            className="bg-blue-500 text-white px-2 py-1 mt-2"
-          >
-            Add Project
-          </button>
-        )}
+          {projects.length < 3 && (
+            <button
+              onClick={addProject}
+              className="bg-blue-500 text-white px-2 w-[150px] rounded py-1 mt-2"
+            >
+              Add Project
+            </button>
+          )}
+        </div>
+        <div class="md:w-[400px] w-[300px] mx-auto flex flex-col  justify-center items-center border p-5 rounded-md border-blue-500 ">
+          <h1 class="text-2xl text-violet-800  mb-4 ">Achievements</h1>
+          {achievements.map((achievement, index) => (
+            <div key={index}>
+              <label>Achievement {index + 1}:</label>
+              <textarea
+                placeholder="Achievement"
+                value={achievement}
+                onChange={e => handleAchievementInputChange(e, index)}
+                className="border border-gray-300 rounded p-2 w-full"
+              ></textarea>
+            </div>
+          ))}
+        </div>
       </div>
-      <button onClick={generatePdf}>Generate Resume PDF</button>
+      <button
+        class="text-2xl text-violet-800 bg-green-400 rounded px-5 p-3  mb-7 "
+        onClick={generatePdf}
+      >
+        Generate Resume PDF
+      </button>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  // ... other webpack configurations ...
+// module.exports = {
+//   // ... other webpack configurations ...
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', // Path to your HTML template
-      filename: 'index.html', // Output HTML file name
-    }),
-    // ... other plugins ...
-  ],
-};
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html', // Path to your HTML template
+//       filename: 'index.html', // Output HTML file name
+//     }),
+//     // ... other plugins ...
+//   ],
+// };
